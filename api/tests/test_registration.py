f54485443609bd3701099f4311b6831dbe0f4adb
Anthony Lukach <anthonylukach@gmail.com>
2022-05-03 11:14:29 -0700

    Continue test buildout

--- api/tests/test_registration.py
+++ api/tests/test_registration.py
@@ -1,11 +1,7 @@
-from __future__ import barry_as_FLUFL
-from cmath import exp
 from datetime import timedelta
 import json
 import base64
-from typing import TYPE_CHECKING, Any, Dict
-
-from boto3.dynamodb import conditions
+from typing import TYPE_CHECKING
 
 
 if TYPE_CHECKING:
@@ -21,6 +17,7 @@ def test_list(
     example_ingestion: "schemas.Ingestion",
 ):
     mock_table.put_item(Item=example_ingestion.dynamodb_dict())
+
     response = api_client.get(ingestion_endpoint)
     assert response.status_code == 200
     assert response.json() == {
@@ -43,16 +40,46 @@ def test_list_next_response(
         example_ingestions.append(ingestion)
 
     limit = 25
-    response = api_client.get(ingestion_endpoint, params={"limit": limit})
-    assert response.status_code == 200
     expected_next = json.loads(
         example_ingestions[limit - 1].json(
             include={"created_by", "id", "status", "created_at"}
         )
     )
 
+    response = api_client.get(ingestion_endpoint, params={"limit": limit})
+    assert response.status_code == 200
     assert json.loads(base64.b64decode(response.json()["next"])) == expected_next
     assert response.json()["items"] == [
         json.loads(ingestion.json(by_alias=True))
         for ingestion in example_ingestions[:limit]
+    ]
+
+
+def test_list_next_page(
+    api_client: "TestClient",
+    mock_table: "services.Table",
+    example_ingestion: "schemas.Ingestion",
+):
+    example_ingestions = []
+    for i in range(100):
+        ingestion = example_ingestion.copy()
+        ingestion.id = str(i)
+        ingestion.created_at = ingestion.created_at + timedelta(hours=i)
+        mock_table.put_item(Item=ingestion.dynamodb_dict())
+        example_ingestions.append(ingestion)
+
+    limit = 25
+    next_param = base64.b64encode(
+        example_ingestions[limit - 1]
+        .json(include={"created_by", "id", "status", "created_at"})
+        .encode()
+    )
+
+    response = api_client.get(
+        ingestion_endpoint, params={"limit": limit, "next": next_param}
+    )
+    assert response.status_code == 200
+    assert response.json()["items"] == [
+        json.loads(ingestion.json(by_alias=True))
+        for ingestion in example_ingestions[limit : limit * 2]
     ]
