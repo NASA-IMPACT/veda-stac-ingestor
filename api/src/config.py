12d3d6e4e4dc41ae2f65f3ed86e3d4cd49164534
Anthony Lukach <anthonylukach@gmail.com>
2022-07-08 14:49:58 -0700

    Use AnyHttpUrl to allow localhost

--- api/src/config.py
+++ api/src/config.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseSettings, Field, HttpUrl
+from pydantic import BaseSettings, Field, AnyHttpUrl
 from pydantic_ssm_settings import AwsSsmSourceConfig
 
 
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
 
     root_path: Optional[str] = Field(description="Path from where to serve this URL.")
 
-    jwks_url: HttpUrl = Field(
+    jwks_url: AnyHttpUrl = Field(
         description="URL of JWKS, e.g. https://cognito-idp.{region}.amazonaws.com/{userpool_id}/.well-known/jwks.json"  # noqa
     )
 
