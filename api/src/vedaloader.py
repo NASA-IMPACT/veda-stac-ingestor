3a3bc8b1ec5f71435c370a06a21406c3af788488
Nathan Zimmerman <npzimmerman@gmail.com>
2022-09-19 16:41:40 -0500

    Lint

--- api/src/vedaloader.py
+++ api/src/vedaloader.py
@@ -9,10 +9,7 @@ logger = logging.getLogger(__name__)
 class VEDALoader(Loader):
     """Utilities for loading data and updating collection summaries/extents."""
 
-    def update_collection_summaries(
-        self,
-        collection_id: str
-    ) -> None:
+    def update_collection_summaries(self, collection_id: str) -> None:
         """Update collection-level summaries for a single collection.
         This includes dashboard summaries (i.e. datetime and cog_default) as well as
         STAC-conformant bbox and temporal extent."""
@@ -21,18 +18,20 @@ class VEDALoader(Loader):
         conn = self.db.connect()
         with conn.cursor() as cur:
             with conn.transaction():
-                logger.info("Updating dashboard summaries for collection: {}.".format(collection_id))
+                logger.info(
+                    "Updating dashboard summaries for collection: {}.".format(
+                        collection_id
+                    )
+                )
                 cur.execute(
                     "SELECT dashboard.update_collection_default_summaries(%s)",
-                    collection_id
+                    collection_id,
                 )
                 logger.info("Updating bbox for collection: {}.".format(collection_id))
-                cur.execute(
-                    "SELECT pgstac.collection_bbox(%s)",
-                    collection_id
+                cur.execute("SELECT pgstac.collection_bbox(%s)", collection_id)
+                logger.info(
+                    "Updating temporal extent for collection: {}.".format(collection_id)
                 )
-                logger.info("Updating temporal extent for collection: {}.".format(collection_id))
                 cur.execute(
-                    "SELECT pgstac.collection_temporal_extent(%s)",
-                    collection_id
+                    "SELECT pgstac.collection_temporal_extent(%s)", collection_id
                 )
