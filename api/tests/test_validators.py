52a08798f9f3f79c194916b3c89a7ffdb933f2c2
ividito <isayah@developmentseed.org>
2023-02-06 21:42:44 -0400

    Fix schema - "null" no longer accepted

--- api/tests/test_validators.py
+++ api/tests/test_validators.py
@@ -9,7 +9,7 @@ sample_data = {
     "description": "short description",
     "license": "CC0",
     "is_periodic": False,
-    "time_density": "null",
+    "time_density": None,
     "spatial_extent": {"xmin": -180, "ymin": -90, "xmax": 180, "ymax": 90},
     "temporal_extent": {
         "startdate": "2021-08-14T00:00:00Z",
